d52259bac0962653ba8a8b17050a79eafb90a6ab
BeckySosa <156535491+BeckySosa@users.noreply.github.com>
2024-05-21 15:52:28 -0300

    Create InscripcionData.java

---
+++ src/universidadgrupo6/accesoADatos/InscripcionData.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package universidadgrupo6.accesoADatos;
+
+import java.sql.*;
+
+public class InscripcionData {
+    private Connection con;
+    private MateriaData matData;
+    private AlumnoData aluData;
+
+    public InscripcionData() {
+    }
+    public void guardarInscripcion(inscripcion insc){
+        String sql="INSERT INTO inscripcion "
+        
+    }
+}
